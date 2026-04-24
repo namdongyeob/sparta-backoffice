86411abb4c18956c54e5a6e4e0e87ff717613293
namdongyeob <ehdduq1324@gmail.com>
2026-04-24 22:21:57 +0900

    feat: 관리자 로그인 기능 추가
    - 이메일/비밀번호
    - 로그인 기능 구현
    - 세션에 관리자
    - 정보 저장 처리
    - 상태별 예외

--- src/main/java/com/sparta/backoffice/admin/service/AdminService.java
+++ src/main/java/com/sparta/backoffice/admin/service/AdminService.java
@@ -3,6 +3,7 @@ package com.sparta.backoffice.admin.service;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.sparta.backoffice.admin.dto.AdminLoginRequest;
 import com.sparta.backoffice.admin.dto.AdminSignupResponse;
 import com.sparta.backoffice.admin.dto.AdminSignupRequest;
 import com.sparta.backoffice.admin.entity.Admin;
@@ -26,5 +27,21 @@ public class AdminService {
 		Admin admin = request.toEntity(encodedPassword);
 		Admin savedAdmin = adminRepository.save(admin);
 		return AdminSignupResponse.from(savedAdmin);
+	}
+	@Transactional(readOnly = true)
+	public Admin login(AdminLoginRequest request) {
+		Admin admin = adminRepository.findByEmail(request.getEmail()).orElseThrow(
+			() -> new IllegalArgumentException("이메일 또는 비밀번호가 올바르지 않습니다."));
+		if (!passwordEncoder.matches(request.getPassword(), admin.getPassword())){
+			throw new IllegalArgumentException("이메일 또는 비밀번호가 올바르지 않습니다.");
+		}
+		switch (admin.getStatus()){
+			case PENDING -> throw new IllegalArgumentException("승인 대기 중인 계정입니다.");
+			case REJECTED -> throw new IllegalArgumentException("거부된 계정입니다.");
+			case SUSPENDED -> throw new IllegalArgumentException("정지된 계정입니다.");
+			case INACTIVE -> throw new IllegalArgumentException("비활성화된 계정입니다.");
+		}
+		return admin;
+
 	}
 }
