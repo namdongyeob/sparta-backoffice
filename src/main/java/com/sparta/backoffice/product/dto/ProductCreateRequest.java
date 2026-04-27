f2016a1e22d2add15b25cb4ba6f372c5a1343888
WookJae Jeong <dnrwo09@gmail.com>
2026-04-27 14:50:05 +0900

    Update ProductCreateRequest.java

--- src/main/java/com/sparta/backoffice/product/dto/ProductCreateRequest.java
+++ src/main/java/com/sparta/backoffice/product/dto/ProductCreateRequest.java
@@ -9,8 +9,10 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class ProductCreateRequest {
 
 	@NotBlank(message = "상품명은 필수값입니다.")
