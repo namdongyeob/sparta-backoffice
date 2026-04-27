0714eeac0c590e0c029a3b98ce98013638b31f05
eashhyeon3232 <jihyeon-lou3232@naver.com>
2026-04-27 16:02:02 +0900

    feat : 고객 정보 수정 기능
    - Customer 엔티티 전화번호 unique=true 추가
    - @PatchMapping을 이용한 특정 고객 정보 업데이트 API 추가
    - @Valid를 이용한 수정 요청 데이터 유효성 검사 적용
    - 중복 데이터 입력 시 IllegalArgumentException 예외 처리

--- src/main/java/com/sparta/backoffice/customer/repository/CustomerRepository.java
+++ src/main/java/com/sparta/backoffice/customer/repository/CustomerRepository.java
@@ -9,8 +9,6 @@ import org.springframework.data.repository.query.Param;
 import com.sparta.backoffice.customer.entity.Customer;
 import com.sparta.backoffice.customer.enums.CustomerStatus;
 
-import jakarta.validation.constraints.Pattern;
-
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 	// JPQL을 사용하여 이름 또는 이메일에 키워드가 포함된 데이터를 조회
@@ -26,6 +24,5 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
 
 	boolean existsByEmail(String email);
 
-	boolean existsByPhoneNumber(
-		@Pattern(regexp = "^010-\\d{4}-\\d{4}$", message = "전화번호 형식은 010-XXXX-XXXX여야 합니다.") String phoneNumber);
+	boolean existsByPhoneNumber(String phoneNumber);
 }
