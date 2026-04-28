e9ee4f6ba6393218f2e08a19d8193933f70cca73
eashhyeon3232 <jihyeon-lou3232@naver.com>
2026-04-28 16:45:48 +0900

    feat : 고객 삭제 기능 구현(Soft Delete 적용)
    - @SQLDelete 및 @SQLRestriction을 활용한 삭제 로직 구현
    - 삭제 시 'deleted_at' 컬럼에 현재 시간 기록 및 조회 시 필터링 처리
    - 존재하지 않는 고객 삭제 시도 시 예외 처리 추가
    - 성공 시 204 No Content 상태 코드 반환

--- src/main/java/com/sparta/backoffice/customer/controller/CustomerController.java
+++ src/main/java/com/sparta/backoffice/customer/controller/CustomerController.java
@@ -68,4 +68,5 @@ public class CustomerController {
 		return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
 	}
 
+
 }
--- src/main/java/com/sparta/backoffice/customer/entity/Customer.java
+++ src/main/java/com/sparta/backoffice/customer/entity/Customer.java
@@ -22,7 +22,6 @@ import lombok.NoArgsConstructor;
 @Entity
 @Table(name = "customers")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-//@SoftDelete(columnName = "is_delete")
 @SQLDelete(sql = "UPDATE customers SET deleted_at = NOW() WHERE id = ?")  // 소프트딜리트는 시간기록이 안됨  지금 사용한 방식이 삭제 조회 가능
 @SQLRestriction("deleted_at IS NULL")  // 삭제시간이 null인지 아닌지
 public class Customer extends BaseEntity {
@@ -54,4 +53,5 @@ public class Customer extends BaseEntity {
 	public void updateStatus(CustomerStatus status) {
 		this.status = status;
 	}
+
 }
