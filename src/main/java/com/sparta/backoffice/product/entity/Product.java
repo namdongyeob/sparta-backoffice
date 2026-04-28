89a5b615f1e1d9fa7294295ee19d8ab39797de5e
WookJae Jeong <dnrwo09@gmail.com>
2026-04-28 15:16:45 +0900

    fix: decreaseStock 메서드 로직 수정
    - 단종/품절 상품 주문 제한 로직 추가

--- src/main/java/com/sparta/backoffice/product/entity/Product.java
+++ src/main/java/com/sparta/backoffice/product/entity/Product.java
@@ -137,14 +137,26 @@ public class Product extends BaseEntity {
 	}
 
 	public void decreaseStock(int quantity) {
+		if (this.status == ProductStatus.DISCONTINUED) {
+			throw new IllegalStateException("단종 상품은 주문할 수 없습니다.");
+		}
+
+		if (this.status == ProductStatus.SOLD_OUT) {
+			throw new IllegalStateException("품절 상품은 주문할 수 없습니다.");
+		}
+
+		if (quantity <= 0) {
+			throw new IllegalArgumentException("수량은 1개 이상이어야 합니다.");
+		}
+
 		if (this.stock < quantity) {
 			throw new IllegalArgumentException("재고가 부족합니다.");
 		}
 
 		this.stock -= quantity;
 
-		if (this.status != ProductStatus.DISCONTINUED) {
-			this.status = determineStatus(this.stock);
+		if (this.stock == 0) {
+			this.status = ProductStatus.SOLD_OUT;
 		}
 	}
 }
