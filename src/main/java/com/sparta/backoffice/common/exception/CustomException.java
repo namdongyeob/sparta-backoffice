e2267166eb56c6abdf884e5ffe0bbed53c9fb750
WookJaes <dnrwo09@gmail.com>
2026-04-29 19:56:16 +0900

    feat: 전역 예외 처리 구현
    - ErrorCode enum을 사용해 예외 응답을 일관되게 처리하도록 수정
    - CustomException을 적용하여 전역 예외 처리 방식 통일
    - Admin, Product, Customer 등 도메인별 에러 코드 추가

--- src/main/java/com/sparta/backoffice/common/exception/CustomException.java
+++ src/main/java/com/sparta/backoffice/common/exception/CustomException.java
@@ -1,16 +1,20 @@
 package com.sparta.backoffice.common.exception;
 
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
+
+import lombok.Getter;
 
 @Getter
 public class CustomException extends RuntimeException {
-    private final HttpStatus status;
-    private final String message;
 
-    public CustomException(HttpStatus status, String message) {
-        super(message);
-        this.status = status;
-        this.message = message;
-    }
+	private final ErrorCode errorCode;
+
+	public CustomException(ErrorCode errorCode) {
+		super(errorCode.getMessage());
+		this.errorCode = errorCode;
+	}
+
+	public HttpStatus getStatus() {
+		return errorCode.getStatus();
+	}
 }
