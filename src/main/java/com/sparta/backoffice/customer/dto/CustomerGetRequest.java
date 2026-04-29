2454e7f6c3478258240c8830caf1a02e6654bfc7
eashhyeon3232 <jihyeon-lou3232@naver.com>
2026-04-29 17:54:42 +0900

    refactor : customer 코드 리팩토링
    - CustomerGetRequest getPageable() 메서드 추가 및 @Setter 제거
    - CustomerService 페이징 로직 제거

--- src/main/java/com/sparta/backoffice/customer/dto/CustomerGetRequest.java
+++ src/main/java/com/sparta/backoffice/customer/dto/CustomerGetRequest.java
@@ -1,20 +1,52 @@
 package com.sparta.backoffice.customer.dto;
 
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
 import com.sparta.backoffice.customer.enums.CustomerStatus;
 
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
-@NoArgsConstructor
 public class CustomerGetRequest {
-	private String keyword;
-	private int page = 1;
-	private int size = 10;
-	private String sortBy;  // 기본값 가입일
-	private String sortOrder;
-	private CustomerStatus status;
+	private String keyword;       // 검색 키워드 (주문번호, 고객명)
+	private int page = 1;         // 페이지 번호 (기본값: 1)
+	private int size = 10;        // 페이지당 개수 (기본값: 10)
+	private String sortBy;        // 정렬 기준
+	private String sortOrder;     // 정렬 순서 (asc, desc)
+	private CustomerStatus status;   // 상태 필터
 
+	public CustomerGetRequest(CustomerStatus status, String sortOrder, String sortBy, int size, int page,
+		String keyword) {
+		this.status = status;
+		this.sortOrder = sortOrder;
+		this.sortBy = sortBy;
+		this.size = size;
+		this.page = page;
+		this.keyword = keyword;
+	}
+
+	public Pageable getPageable() {
+		Sort sort;
+
+		if (sortOrder == null || sortOrder.isBlank()) {
+			sort = Sort.by(
+				Sort.Order.desc("createdAt")
+			);
+		} else {
+			Sort.Direction sortDirection =
+				"asc".equalsIgnoreCase(sortOrder)
+					? Sort.Direction.ASC
+					: Sort.Direction.DESC;
+
+			sort = Sort.by(sortDirection, sortBy);
+		}
+
+		return PageRequest.of(
+			page - 1,
+			size,
+			sort
+		);
+	}
 }
