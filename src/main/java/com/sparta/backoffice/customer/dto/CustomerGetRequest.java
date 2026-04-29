4e77db9e3f050a5b7ac6c68662337857a9625fe0
namdongyeob <ehdduq1324@gmail.com>
2026-04-29 23:31:33 +0900

    refactor: CustomerGetRequest 생성자 제거 및 @NoArgsConstructor 도입
    기본 생성자를 통해 Spring의 데이터 바인딩이 가능하도록 개선
    
    파라미터 누락 시 int 타입 변환 에러(400 Bad Request) 및 NPE 해결

--- src/main/java/com/sparta/backoffice/customer/dto/CustomerGetRequest.java
+++ src/main/java/com/sparta/backoffice/customer/dto/CustomerGetRequest.java
@@ -7,8 +7,10 @@ import org.springframework.data.domain.Sort;
 import com.sparta.backoffice.customer.enums.CustomerStatus;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@NoArgsConstructor
 public class CustomerGetRequest {
 	private String keyword;       // 검색 키워드 (주문번호, 고객명)
 	private int page = 1;         // 페이지 번호 (기본값: 1)
@@ -16,16 +18,6 @@ public class CustomerGetRequest {
 	private String sortBy;        // 정렬 기준
 	private String sortOrder;     // 정렬 순서 (asc, desc)
 	private CustomerStatus status;   // 상태 필터
-
-	public CustomerGetRequest(CustomerStatus status, String sortOrder, String sortBy, int size, int page,
-		String keyword) {
-		this.status = status;
-		this.sortOrder = sortOrder;
-		this.sortBy = sortBy;
-		this.size = size;
-		this.page = page;
-		this.keyword = keyword;
-	}
 
 	public Pageable getPageable() {
 		Sort sort;
